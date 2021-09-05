d7987a43466ccec720c01a52be0fe667eed3a9f3
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2021-09-05 14:26:05 +0300

    Structured meshes can now map points from global cartesian to isoparametric coordinate systems.

--- src/MGroup.MSolve.Meshes/Structured/ICartesianMesh.cs
+++ src/MGroup.MSolve.Meshes/Structured/ICartesianMesh.cs
@@ -6,11 +6,41 @@ using System.Text;
 //      ISimplicialStructuredMesh. There is actually a lot of math behind these: https://en.wikipedia.org/wiki/Simplex.
 namespace MGroup.MSolve.Meshes.Structured
 {
-    /// <summary>
-    /// Special case of structured mesh, where elements are quadrilaterals in 2D or hexahedrals in 3D.
-    /// </summary>
-    public interface ICartesianMesh : IStructuredMesh
-    {
-        int[] NumElements { get; }
-    }
+	/// <summary>
+	/// Special case of structured mesh, where elements are quadrilaterals in 2D or hexahedrals in 3D.
+	/// </summary>
+	public interface ICartesianMesh : IStructuredMesh
+	{
+		double[] DistancesBetweenPoints { get; }
+
+		int[] NumElements { get; }
+
+		public (int elementID, double[] naturalCoords) FindElementContaining(double[] globalCoords)
+		{
+			int dim = Dimension;
+			var elementIdx = new int[dim];
+			var naturalCoords = new double[dim];
+			for (int d = 0; d < dim; ++d)
+			{
+				// For a given axis:
+				// Let x be the global cartesian coordinate of a point along this axis.
+				// Let xMin be the lower bound of x.
+				// Let e be the index of the element that contains x along this axis.
+				// Let xi be the isoparametric coordinate of element e, that corresponds to x.
+				// Let dx be the length of each element along this axis.
+				// Let xStart = xMin + e*dx and xEnd = xMin + (e+1)*dx be the global cartesian coordinates of the start 
+				// and end point of element e.
+				// Then x = xMin + xStart * 1/2 * (1-xi) + xEnd * 1/2 * (1+xi) = ... = xMin + e*dx + dx * (1+xi)/2 
+				// <=> x = xMin + xStart + dx * (1+xi)/2 <=> xi = 2*(x-xMin)/dx -2*e -1 
+				// where dx * (1+xi)/2 belongs in [0, dx]
+				double x = globalCoords[d];
+				double xMin = MinCoordinates[d];
+				double dx = DistancesBetweenPoints[d];
+				elementIdx[d] = (int)Math.Floor((x - xMin) / dx);
+				naturalCoords[d] = 2 * (x - xMin) / dx - 2 * elementIdx[d] - 1;
+			}
+
+			return (GetElementID(elementIdx), naturalCoords);
+		}
+	}
 }
