715e60c26f80e3997160fdc64cdc52eaa762c6db
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2021-11-19 21:20:31 +0200

    Added new projects

---
+++ src/MGroup.XFEM.IsoXFEM/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MGroup.XFEM.IsoXFEM
+{
+	public class Class1
+	{
+	}
+}
---
+++ tests/MGroup.XFEM.IsoXFEM.Tests/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MGroup.XFEM.IsoXFEM.Tests
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
