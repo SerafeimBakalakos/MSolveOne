e8f9b3b592c09feba0e16733c275ba6e3d96ab23
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2021-12-01 15:09:11 +0200

    Added tests for FETI-DP

---
+++ tests/MGroup.Solvers.DDM.Tests/FetiDP/SimpleFetiDPSolverTests.cs
@@ -0,0 +1,229 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using MGroup.Constitutive.Thermal;
+using MGroup.Environments;
+using MGroup.LinearAlgebra.Distributed.IterativeMethods;
+using MGroup.LinearAlgebra.Distributed.IterativeMethods.PCG;
+using MGroup.LinearAlgebra.Distributed.IterativeMethods.PCG.Reorthogonalization;
+using MGroup.LinearAlgebra.Iterative;
+using MGroup.LinearAlgebra.Iterative.Termination;
+using MGroup.LinearAlgebra.Matrices;
+using MGroup.MSolve.DataStructures;
+using MGroup.MSolve.Discretization;
+using MGroup.MSolve.Solution;
+using MGroup.NumericalAnalyzers;
+using MGroup.Solvers.DDM.FetiDP;
+using MGroup.Solvers.DDM.FetiDP.CoarseProblem;
+using MGroup.Solvers.DDM.FetiDP.Dofs;
+using MGroup.Solvers.DDM.FetiDP.InterfaceProblem;
+using MGroup.Solvers.DDM.FetiDP.StiffnessMatrices;
+using MGroup.Solvers.DDM.LinearSystem;
+using MGroup.Solvers.DDM.Psm;
+using MGroup.Solvers.DDM.PSM.InterfaceProblem;
+using MGroup.Solvers.DDM.PSM.StiffnessMatrices;
+using MGroup.Solvers.DDM.Tests.ExampleModels;
+using MGroup.Solvers.DofOrdering;
+using MGroup.Solvers.Results;
+using Xunit;
+
+namespace MGroup.Solvers.DDM.Tests.FetiDP
+{
+	public static class SimpleFetiDPSolverTests
+	{
+		[Theory]
+		[InlineData(EnvironmentChoice.SequentialShared, false, false, false)]
+		[InlineData(EnvironmentChoice.SequentialShared, true, false, false)]
+		[InlineData(EnvironmentChoice.SequentialShared, true, true, false)]
+		[InlineData(EnvironmentChoice.SequentialShared, true, false, true)]
+		[InlineData(EnvironmentChoice.SequentialShared, true, true, true)]
+		[InlineData(EnvironmentChoice.TplShared, false, false, false)]
+		[InlineData(EnvironmentChoice.TplShared, true, false, false)]
+		[InlineData(EnvironmentChoice.TplShared, true, true, false)]
+		[InlineData(EnvironmentChoice.TplShared, true, false, true)]
+		[InlineData(EnvironmentChoice.TplShared, true, true, true)]
+		public static void TestForBrick3D(EnvironmentChoice env, bool coarseDistributed, bool coarseJacobi, bool coarseReortho)
+			=> TestForBrick3DInternal(env.CreateEnvironment(), coarseDistributed, coarseJacobi, coarseReortho);
+
+		internal static void TestForBrick3DInternal(IComputeEnvironment environment, bool isCoarseProblemDistributed,
+			bool useCoarseJacobiPreconditioner, bool useReorthogonalizedPcg)
+		{
+			// Environment
+			ComputeNodeTopology nodeTopology = Brick3DExample.CreateNodeTopology();
+			environment.Initialize(nodeTopology);
+
+			// Model
+			IModel model = Brick3DExample.CreateMultiSubdomainModel();
+			model.ConnectDataStructures(); //TODOMPI: this is also done in the analyzer
+			ICornerDofSelection cornerDofs = Brick3DExample.GetCornerDofs(model);
+
+			// Solver
+			var solverFactory = new FetiDPSolver<SymmetricCscMatrix>.Factory(
+				environment, cornerDofs, new FetiDPSubdomainMatrixManagerSymmetricCSparse.Factory());
+
+			solverFactory.InterfaceProblemSolverFactory = new FetiDPInterfaceProblemSolverFactoryPcg()
+			{
+				MaxIterations = 200,
+				ResidualTolerance = 1E-10
+			};
+
+			if (isCoarseProblemDistributed)
+			{
+				var coarseProblemFactory = new FetiDPCoarseProblemDistributed.Factory();
+				if (useReorthogonalizedPcg)
+				{
+					var coarseProblemPcgBuilder = new ReorthogonalizedPcg.Builder();
+					coarseProblemPcgBuilder.MaxIterationsProvider = new FixedMaxIterationsProvider(200);
+					coarseProblemPcgBuilder.ResidualTolerance = 2E-12;
+					coarseProblemPcgBuilder.DirectionVectorsRetention = new FixedDirectionVectorsRetention(40, true);
+					coarseProblemFactory.CoarseProblemSolver = coarseProblemPcgBuilder.Build();
+				}
+				else
+				{
+					var coarseProblemPcgBuilder = new PcgAlgorithm.Builder();
+					coarseProblemPcgBuilder.MaxIterationsProvider = new FixedMaxIterationsProvider(200);
+					coarseProblemPcgBuilder.ResidualTolerance = 2E-12;
+					coarseProblemFactory.CoarseProblemSolver = coarseProblemPcgBuilder.Build();
+				}
+
+				coarseProblemFactory.UseJacobiPreconditioner = useCoarseJacobiPreconditioner;
+				solverFactory.CoarseProblemFactory = coarseProblemFactory;
+			}
+			else 
+			{
+				var coarseProblemMatrix = new FetiDPCoarseProblemMatrixSymmetricCSparse();
+				solverFactory.CoarseProblemFactory = new FetiDPCoarseProblemGlobal.Factory(coarseProblemMatrix);
+			}
+
+			DistributedAlgebraicModel<SymmetricCscMatrix> algebraicModel = solverFactory.BuildAlgebraicModel(model);
+			FetiDPSolver<SymmetricCscMatrix> solver = solverFactory.BuildSolver(model, algebraicModel);
+
+			// Linear static analysis
+			var problem = new ProblemThermal(model, algebraicModel, solver);
+			var childAnalyzer = new LinearAnalyzer(model, algebraicModel, solver, problem);
+			var parentAnalyzer = new StaticAnalyzer(model, algebraicModel, solver, problem, childAnalyzer);
+
+			// Run the analysis
+			parentAnalyzer.Initialize();
+			parentAnalyzer.Solve();
+
+			// Check results
+			NodalResults expectedResults = Brick3DExample.GetExpectedNodalValues(model.AllDofs);
+			double tolerance = 1E-7;
+			environment.DoPerNode(subdomainID =>
+			{
+				NodalResults computedResults = algebraicModel.ExtractAllResults(subdomainID, solver.LinearSystem.Solution);
+				Assert.True(expectedResults.IsSuperSetOf(computedResults, tolerance, out string msg), msg);
+			});
+
+			//Debug.WriteLine($"Num PCG iterations = {solver.PcgStats.NumIterationsRequired}," +
+			//    $" final residual norm ratio = {solver.PcgStats.ResidualNormRatioEstimation}");
+
+			// Check convergence
+			int precision = 10;
+			int pcgIterationsExpected = 29;
+			double pcgResidualNormRatioExpected = 6.641424316172292E-11;
+			IterativeStatistics stats = solver.InterfaceProblemSolutionStats;
+			Assert.Equal(pcgIterationsExpected, stats.NumIterationsRequired);
+			Assert.Equal(pcgResidualNormRatioExpected, stats.ResidualNormRatioEstimation, precision);
+		}
+
+		[Theory]
+		[InlineData(EnvironmentChoice.SequentialShared, false, false, false)]
+		//[InlineData(EnvironmentChoice.SequentialShared, true, false, false)]
+		//[InlineData(EnvironmentChoice.SequentialShared, true, true, false)]
+		//[InlineData(EnvironmentChoice.SequentialShared, true, false, true)]
+		//[InlineData(EnvironmentChoice.SequentialShared, true, true, true)]
+		[InlineData(EnvironmentChoice.TplShared, false, false, false)]
+		//[InlineData(EnvironmentChoice.TplShared, true, false, false)]
+		//[InlineData(EnvironmentChoice.TplShared, true, true, false)]
+		//[InlineData(EnvironmentChoice.TplShared, true, false, true)]
+		//[InlineData(EnvironmentChoice.TplShared, true, true, true)]
+		public static void TestForPlane2D(EnvironmentChoice env, bool coarseDistributed, bool coarseJacobi, bool coarseReortho)
+			=> TestForPlane2DInternal(env.CreateEnvironment(), coarseDistributed, coarseJacobi, coarseReortho);
+
+		internal static void TestForPlane2DInternal(IComputeEnvironment environment, bool isCoarseProblemDistributed,
+			bool useCoarseJacobiPreconditioner, bool useReorthogonalizedPcg)
+		{
+			// Environment
+			ComputeNodeTopology nodeTopology = Plane2DExample.CreateNodeTopology();
+			environment.Initialize(nodeTopology);
+
+			// Model
+			IModel model = Plane2DExample.CreateMultiSubdomainModel();
+			model.ConnectDataStructures(); //TODOMPI: this is also done in the analyzer
+			ICornerDofSelection cornerDofs = Plane2DExample.GetCornerDofs(model);
+
+			// Solver
+			var solverFactory = new FetiDPSolver<SymmetricCscMatrix>.Factory(
+				environment, cornerDofs, new FetiDPSubdomainMatrixManagerSymmetricCSparse.Factory());
+
+			solverFactory.InterfaceProblemSolverFactory = new FetiDPInterfaceProblemSolverFactoryPcg()
+			{
+				MaxIterations = 200,
+				ResidualTolerance = 1E-10
+			};
+
+			if (isCoarseProblemDistributed)
+			{
+				var coarseProblemFactory = new FetiDPCoarseProblemDistributed.Factory();
+				if (useReorthogonalizedPcg)
+				{
+					var coarseProblemPcgBuilder = new ReorthogonalizedPcg.Builder();
+					coarseProblemPcgBuilder.DirectionVectorsRetention = new FixedDirectionVectorsRetention(30, true);
+					coarseProblemPcgBuilder.MaxIterationsProvider = new FixedMaxIterationsProvider(200);
+					coarseProblemPcgBuilder.ResidualTolerance = 2E-12;
+					coarseProblemFactory.CoarseProblemSolver = coarseProblemPcgBuilder.Build();
+				}
+				else
+				{
+					var coarseProblemPcgBuilder = new PcgAlgorithm.Builder();
+					coarseProblemPcgBuilder.MaxIterationsProvider = new FixedMaxIterationsProvider(200);
+					coarseProblemPcgBuilder.ResidualTolerance = 2E-12;
+					coarseProblemFactory.CoarseProblemSolver = coarseProblemPcgBuilder.Build();
+				}
+				
+				coarseProblemFactory.UseJacobiPreconditioner = useCoarseJacobiPreconditioner;
+				solverFactory.CoarseProblemFactory = coarseProblemFactory;
+			}
+			else 
+			{
+				var coarseProblemMatrix = new FetiDPCoarseProblemMatrixSymmetricCSparse();
+				solverFactory.CoarseProblemFactory = new FetiDPCoarseProblemGlobal.Factory(coarseProblemMatrix);
+			}
+
+			DistributedAlgebraicModel<SymmetricCscMatrix> algebraicModel = solverFactory.BuildAlgebraicModel(model);
+			FetiDPSolver<SymmetricCscMatrix> solver = solverFactory.BuildSolver(model, algebraicModel);
+
+			// Linear static analysis
+			var problem = new ProblemThermal(model, algebraicModel, solver);
+			var childAnalyzer = new LinearAnalyzer(model, algebraicModel, solver, problem);
+			var parentAnalyzer = new StaticAnalyzer(model, algebraicModel, solver, problem, childAnalyzer);
+
+			// Run the analysis
+			parentAnalyzer.Initialize();
+			parentAnalyzer.Solve();
+
+			// Check results
+			NodalResults expectedResults = Plane2DExample.GetExpectedNodalValues(model.AllDofs);
+			double tolerance = 1E-7;
+			environment.DoPerNode(subdomainID =>
+			{
+				NodalResults computedResults = algebraicModel.ExtractAllResults(subdomainID, solver.LinearSystem.Solution);
+				Assert.True(expectedResults.IsSuperSetOf(computedResults, tolerance, out string msg), msg);
+			});
+
+			//Debug.WriteLine($"Num PCG iterations = {solver.PcgStats.NumIterationsRequired}," +
+			//    $" final residual norm ratio = {solver.PcgStats.ResidualNormRatioEstimation}");
+
+			// Check convergence
+			int precision = 10;
+			int pcgIterationsExpected = 14;
+			double pcgResidualNormRatioExpected = 2.868430313362798E-11;
+			IterativeStatistics stats = solver.InterfaceProblemSolutionStats;
+			Assert.Equal(pcgIterationsExpected, stats.NumIterationsRequired);
+			Assert.Equal(pcgResidualNormRatioExpected, stats.ResidualNormRatioEstimation, precision);
+		}
+	}
+}
