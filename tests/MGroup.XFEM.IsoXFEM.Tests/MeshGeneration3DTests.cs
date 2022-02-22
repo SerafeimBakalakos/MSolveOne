7159f5b5e3fce8e15eddb5666e6e5c2be5d90f7b
effie <effiechroni@gmail.com>
2022-02-22 20:11:26 +0200

    Add X-Y-Z Test.

--- tests/MGroup.XFEM.IsoXFEM.Tests/MeshGeneration3DTests.cs
+++ tests/MGroup.XFEM.IsoXFEM.Tests/MeshGeneration3DTests.cs
@@ -13,6 +13,7 @@ namespace MGroup.XFEM.IsoXFEM.Tests
 
 	public class MeshGeneration3DTests
 	{
+		#region Z-Y-X
 		[Fact]
 		private void CreateNodes3DTest()
 		{
@@ -171,5 +172,166 @@ namespace MGroup.XFEM.IsoXFEM.Tests
 				}
 			}
 		}
+		#endregion
+		#region X-Y-Z
+		//[Fact]
+		//private void CreateNodes3DTest()
+		//{
+		//	var geometry = new GeometryProperties(20, 20, 20, new int[] { 2, 2, 2 });
+		//	var material = new ElasticMaterial3D();
+		//	material.YoungModulus = 1;
+		//	material.PoissonRatio = 0.3;
+		//	IMeshGeneration modelWithNodesExpected = new MeshGeneration3D(material, geometry);
+		//	var nodesComputed = modelWithNodesExpected.CreateNodes();
+		//	var nodesExpected = new List<XNode>();
+		//	nodesExpected.Add(new XNode(0, new double[] { 0, 0, 0 }));
+		//	nodesExpected.Add(new XNode(1, new double[] { 10, 0, 0 }));
+		//	nodesExpected.Add(new XNode(2, new double[] { 20, 0, 0 }));
+		//	nodesExpected.Add(new XNode(3, new double[] { 0, 10, 0 }));
+		//	nodesExpected.Add(new XNode(4, new double[] { 10, 10, 0 }));
+		//	nodesExpected.Add(new XNode(5, new double[] { 20, 10, 0 }));
+		//	nodesExpected.Add(new XNode(6, new double[] { 0, 20, 0 }));
+		//	nodesExpected.Add(new XNode(7, new double[] { 10, 20, 0 }));
+		//	nodesExpected.Add(new XNode(8, new double[] { 20, 20, 0 }));
+		//	nodesExpected.Add(new XNode(9, new double[] { 0, 0, 10 }));
+		//	nodesExpected.Add(new XNode(10, new double[] { 10, 0, 10 }));
+		//	nodesExpected.Add(new XNode(11, new double[] { 20, 0, 10 }));
+		//	nodesExpected.Add(new XNode(12, new double[] { 0, 10, 10 }));
+		//	nodesExpected.Add(new XNode(13, new double[] { 10, 10, 10 }));
+		//	nodesExpected.Add(new XNode(14, new double[] { 20, 10, 10 }));
+		//	nodesExpected.Add(new XNode(15, new double[] { 0, 20, 10 }));
+		//	nodesExpected.Add(new XNode(16, new double[] { 10, 20, 10 }));
+		//	nodesExpected.Add(new XNode(17, new double[] { 20, 20, 10 }));
+		//	nodesExpected.Add(new XNode(18, new double[] { 0, 0, 20 }));
+		//	nodesExpected.Add(new XNode(19, new double[] { 10, 0, 20 }));
+		//	nodesExpected.Add(new XNode(20, new double[] { 20, 0, 20 }));
+		//	nodesExpected.Add(new XNode(21, new double[] {  0, 10, 20 }));
+		//	nodesExpected.Add(new XNode(22, new double[] { 10, 10, 20 }));
+		//	nodesExpected.Add(new XNode(23, new double[] { 20, 10, 20 }));
+		//	nodesExpected.Add(new XNode(24, new double[] {  0, 20, 20 }));
+		//	nodesExpected.Add(new XNode(25, new double[] { 10, 20, 20 }));
+		//	nodesExpected.Add(new XNode(26, new double[] { 20, 20, 20 }));
+		//	for (int i = 0; i < nodesExpected.Count; i++)
+		//	{
+		//		Assert.Equal(nodesExpected[i].ID, nodesComputed[i].ID);
+		//		Assert.Equal(nodesExpected[i].X, nodesComputed[i].X);
+		//		Assert.Equal(nodesExpected[i].Y, nodesComputed[i].Y);
+		//		Assert.Equal(nodesExpected[i].Z, nodesComputed[i].Z);
+		//	}
+		//}
+		//[Fact]
+		//private void CreateElements3D()
+		//{
+		//	var geometry = new GeometryProperties(20, 20, 20, new int[] { 2, 2, 2 });
+		//	var material = new ElasticMaterial3D();
+		//	material.YoungModulus = 1;
+		//	material.PoissonRatio = 0.3;
+		//	IMeshGeneration model = new MeshGeneration3D(material, geometry);
+		//	var nodes = model.CreateNodes();
+		//	var elementsComputed = model.CreateElements(nodes);
+		//	var elementsExpected = new List<IsoXfemElement3D>();
+		//	elementsExpected.Add(new IsoXfemElement3D(0, material, geometry, new[]
+		//	{
+		//		nodes[0],
+		//		nodes[1],
+		//		nodes[4],
+		//		nodes[3],
+		//		nodes[9],
+		//		nodes[10],
+		//		nodes[13],
+		//		nodes[12]
+		//	}));
+		//	elementsExpected.Add(new IsoXfemElement3D(1, material, geometry, new[]
+		//	{
+		//		nodes[1],
+		//		nodes[2],
+		//		nodes[5],
+		//		nodes[4],
+		//		nodes[10],
+		//		nodes[11],
+		//		nodes[14],
+		//		nodes[13]
+		//	}));
+		//	elementsExpected.Add(new IsoXfemElement3D(2, material, geometry, new[]
+		//	{
+		//		nodes[3],
+		//		nodes[4],
+		//		nodes[7],
+		//		nodes[6],
+		//		nodes[12],
+		//		nodes[13],
+		//		nodes[16],
+		//		nodes[15]
+		//	}));
+		//	elementsExpected.Add(new IsoXfemElement3D(3, material, geometry, new[]
+		//	{
+		//		nodes[4],
+		//		nodes[5],
+		//		nodes[8],
+		//		nodes[7],
+		//		nodes[13],
+		//		nodes[14],
+		//		nodes[17],
+		//		nodes[16]
+		//	}));
+		//	elementsExpected.Add(new IsoXfemElement3D(4, material, geometry, new[]
+		//	{
+		//		nodes[9],
+		//		nodes[10],
+		//		nodes[13],
+		//		nodes[12],
+		//		nodes[18],
+		//		nodes[19],
+		//		nodes[22],
+		//		nodes[21]
+		//	}));
+		//	elementsExpected.Add(new IsoXfemElement3D(5, material, geometry, new[]
+		//	{
+		//		nodes[10],
+		//		nodes[11],
+		//		nodes[14],
+		//		nodes[13],
+		//		nodes[19],
+		//		nodes[20],
+		//		nodes[23],
+		//		nodes[22]
+		//	}));
+		//	elementsExpected.Add(new IsoXfemElement3D(6, material, geometry, new[]
+		//	{
+		//		nodes[12],
+		//		nodes[13],
+		//		nodes[16],
+		//		nodes[15],
+		//		nodes[21],
+		//		nodes[22],
+		//		nodes[25],
+		//		nodes[24]
+		//	}));
+		//	elementsExpected.Add(new IsoXfemElement3D(7, material, geometry, new[]
+		//	{
+		//		nodes[13],
+		//		nodes[14],
+		//		nodes[17],
+		//		nodes[16],
+		//		nodes[22],
+		//		nodes[23],
+		//		nodes[26],
+		//		nodes[25]
+		//	}));
+		//	for (int i = 0; i < elementsComputed.Count; i++)
+		//	{
+		//		var elementComputed = elementsComputed[i];
+		//		var elementExpected = elementsExpected[i];
+		//		for (int j = 0; j < elementComputed.Nodes.Count; j++)
+		//		{
+		//			Assert.Equal(elementExpected.Nodes[j].ID, elementComputed.Nodes[j].ID);
+		//		}
+		//		for (int j = 0; j < elementComputed.Nodes.Count; j++)
+		//		{
+		//			Assert.Equal(elementExpected.Nodes[j], elementComputed.Nodes[j]);
+		//		}
+		//	}
+		//}
+		#endregion
 	}
 }
