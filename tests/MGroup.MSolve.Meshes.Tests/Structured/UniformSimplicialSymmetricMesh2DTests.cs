511b1badafa8c05b3bebfca4f8fb3415a6960e1a
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2022-02-26 14:47:03 +0200

    Symmetric Tri3 mesh

---
+++ tests/MGroup.MSolve.Meshes.Tests/Structured/UniformSimplicialSymmetricMesh2DTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using MGroup.MSolve.Discretization.Mesh;
+using MGroup.MSolve.Meshes.Output.VTK;
+using MGroup.MSolve.Meshes.Structured;
+using Xunit;
+
+namespace MGroup.MSolve.Meshes.Tests.Structured
+{
+	public static class UniformSimplicialSymmetricMesh2DTests
+	{
+		[Fact]
+		public static void PlotMesh()
+		{
+			var writer = new VtkMeshWriter();
+
+			string path = @"C:\Users\Serafeim\Desktop\DDM\PFETIDP\meshes\symmetric_triangle_mesh2D.vtk";
+			double[] minCoords = { 0, 0 };
+			double[] maxCoords = { 6, 4 };
+			int[] numNodes = { 7, 5 };
+			//var mesh = new UniformSimplicialMesh2DSymmetric.Builder(minCoords, maxCoords, numNodes).SetMajorAxis(0).BuildMesh();
+			var mesh = new UniformSimplicialSymmetricMesh2D.Builder(minCoords, maxCoords, numNodes).BuildMesh();
+			writer.WriteMesh(path, mesh, 2);
+		}
+	}
+}
